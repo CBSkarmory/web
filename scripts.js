bf1236b9a6eb9b98ee9e9c602f63a0119a536df8
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-07-08 12:37:00 -0400

    trying to debug os detecting

--- scripts.js
+++ scripts.js
@@ -32,4 +32,5 @@ window.onload = function(){
     if (opSys == "Windows" || opSys == "Linux") {
         document.getElementById("otherDL").className += " HideMe";
     }
+    alert("OS: "+opSys);
 }
